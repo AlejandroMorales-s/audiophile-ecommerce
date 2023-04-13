6164e29dc3da24621e67a2c222c15d5d3d8c4798
Alejandro <alejandrom.am33@gmail.com>
2023-04-12 19:13:50 -0600

    added async thunk to create a new user

--- src/reducers/auth/authReducer.ts
+++ src/reducers/auth/authReducer.ts
@@ -1,5 +1,27 @@
-import { createSlice, ActionReducerMapBuilder } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  ActionReducerMapBuilder,
+} from "@reduxjs/toolkit";
 import { InitialState, User, UserForm } from "./interfaces";
+import instance from "../../axios";
+
+//* Create user
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async (user: UserForm, thunkAPI) => {
+    try {
+      const { data }: { data: User } = await instance.post(
+        "/auth/register",
+        user
+      );
+
+      return data;
+    } catch (error: any) {
+      throw new Error(error.response.data.message);
+    }
+  }
+);
 
 //* Initial state
 const initialState: InitialState = {
@@ -12,7 +34,19 @@ const options = {
   name: "auth",
   initialState,
   reducers: {},
-  extraReducers: (builder: ActionReducerMapBuilder<InitialState>) => {},
+  extraReducers: (builder: ActionReducerMapBuilder<InitialState>) => {
+    builder.addCase(createUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(createUser.fulfilled, (state, action) => {
+      const { payload } = action;
+      state.isLoading = false;
+      state.user = payload;
+    });
+    builder.addCase(createUser.rejected, (state) => {
+      state.isLoading = false;
+    });
+  },
 };
 
 export const authSlice = createSlice(options);
