35dc2ddbb58f716c1cf91974f3982b7b980edf77
Alejandro <alejandrom.am33@gmail.com>
2023-04-15 16:33:20 -0600

    added navbar categories links

---
+++ src/components/common/navbarPages/NavbarPages.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./NavbarPages.module.scss";
+
+const pages = [
+  { name: "Home", path: "/" },
+  { name: "Headphones", path: "/headphones" },
+  { name: "Speakers", path: "/speakers" },
+  { name: "Earphones", path: "/earphones" },
+];
+
+interface NavbarPagesProps {
+  inHamburgerMenu: boolean;
+}
+
+const NavbarPages: FC<NavbarPagesProps> = ({ inHamburgerMenu }) => {
+  return (
+    <>
+      {pages.map((page, index) => {
+        const { name, path } = page;
+        return (
+          <NavLink
+            style={({ isActive }) => ({
+              color: isActive ? "hsl(22, 65%, 57%)" : "",
+              backgroundColor:
+                isActive && inHamburgerMenu ? "hsla(22, 65%, 57%, 0.25)" : "",
+            })}
+            className={`${styles.link} subtitle`}
+            to={path}
+            key={index}
+          >
+            {name}
+          </NavLink>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavbarPages;
