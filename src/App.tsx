810b6ef53c08907762288dc5ec9a9ea2d5927d91
Alejandro <alejandrom.am33@gmail.com>
2023-04-15 20:37:56 -0600

    logic to redirect to the main page when is logged

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,27 @@ import { selectNotificationStatus } from "./reducers/notification/notificationRe
 import { authenticateSession } from "./reducers/auth/authAsyncThunks";
 //* Components
 import Notification from "./components/common/modals/Notification";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const App = () => {
   const notificationOpen = useSelector(selectNotificationStatus);
 
   const dispatch = useAppDispatch();
 
+  const currentPath = useLocation().pathname;
+
+  const navigate = useNavigate();
+
+  const authRoutes = ["/auth/login", "/auth/sign-up"];
+
   useEffect(() => {
-    dispatch(authenticateSession());
+    dispatch(authenticateSession()).then((res) => {
+      if (!res.payload) return;
+
+      if (authRoutes.includes(currentPath)) {
+        navigate("/");
+      }
+    });
   }, []);
 
   return (
