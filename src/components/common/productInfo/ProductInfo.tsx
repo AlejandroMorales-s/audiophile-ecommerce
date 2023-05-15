e93c23ef7e71fc1ad6c222fbd603dbbcdbc6899f
Alejandro <alejandrom.am33@gmail.com>
2023-05-14 20:20:41 -0600

    updated product info titles

--- src/components/common/productInfo/ProductInfo.tsx
+++ src/components/common/productInfo/ProductInfo.tsx
@@ -12,7 +12,7 @@ const ProductInfo: FC<Props> = ({ product }) => {
   return (
     <article className={styles["product-info-container"]}>
       <div className={styles["product-info-features"]}>
-        <h3>Features</h3>
+        <h5>Features</h5>
         <p
           className="regular-text"
           dangerouslySetInnerHTML={{
@@ -22,7 +22,7 @@ const ProductInfo: FC<Props> = ({ product }) => {
       </div>
 
       <div className={styles["product-info-includes"]}>
-        <h3>In the box</h3>
+        <h5>In the box</h5>
         <ul>
           {includes.map((includeItem, index) => {
             const { item, quantity } = includeItem;
