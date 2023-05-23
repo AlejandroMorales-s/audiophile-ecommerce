5410ce34b2573b34d678988e3bd696e7cae6c589
Alejandro <alejandrom.am33@gmail.com>
2023-05-22 20:02:27 -0600

    fixed order key

--- src/components/layout/order/Order.tsx
+++ src/components/layout/order/Order.tsx
@@ -23,9 +23,9 @@ const Order: FC<ComponentProps> = ({ order }) => {
     <div className={styles.order}>
       <h6>Order id: {id}</h6>
       <div className={styles["order-products"]}>
-        {products.map((product) => (
+        {products.map((product, index) => (
           <ShoppingCartMenuProduct
-            key={product.product_id}
+            key={index}
             inSummary
             shoppingCartProduct={product}
           />
